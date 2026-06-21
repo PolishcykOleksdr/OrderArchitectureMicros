5e3b32394b6fcb8a75ed2f4761285bd074f5d95b
Polischyk_Oleksandr <sashap07072010@gmail.com>
2026-06-21 14:31:28 +0300

    Init commit

---
+++ src/main/java/com/order/platform/orderapimicrs/OrderApiMicrsApplication.java
@@ -0,0 +1,13 @@
+package com.order.platform.orderapimicrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApiMicrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApiMicrsApplication.class, args);
+	}
+
+}
