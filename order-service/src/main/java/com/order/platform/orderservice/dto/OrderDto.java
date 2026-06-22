1c5eb4434327889af892c7dacc44390c49af45d8
Polischyk_Oleksandr <sashap07072010@gmail.com>
2026-06-22 22:02:37 +0300

    Add Dtos attributes

--- order-service/src/main/java/com/order/platform/orderservice/dto/OrderDto.java
+++ order-service/src/main/java/com/order/platform/orderservice/dto/OrderDto.java
@@ -1,11 +1,23 @@
 package com.order.platform.orderservice.dto;
 
+import com.order.platform.orderservice.enums.OrderStatus;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
 /**
  * author: user,
  * date: 21.06.2026
  */
 
 public record OrderDto(
-        // TODO: add needed attributes
+        Long id,
+        Long customerId,
+        String address,
+        BigDecimal totalAmount,
+        String courierName,
+        Integer etaMinutes,
+        OrderStatus orderStatus,
+        Set<OrderItemDto> orderItemDtos
 ) {
 }
--- order-service/src/main/java/com/order/platform/orderservice/dto/OrderItemDto.java
+++ order-service/src/main/java/com/order/platform/orderservice/dto/OrderItemDto.java
@@ -1,10 +1,15 @@
 package com.order.platform.orderservice.dto;
 
+import java.math.BigDecimal;
+
 /**
  * author: user,
  * date: 21.06.2026
  */
 public record OrderItemDto(
-        // TODO: add needed attributes
+        Long id,
+        Long itemId,
+        Integer quantity,
+        BigDecimal priceAtPurchase
 ) {
 }
